5e3cb11a46d40a962b6672262fa0df7ecf5a6540
Waldemar Głaz <w.glaz@cheil.com>
2022-12-16 11:36:49 +0100

    .env removed

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -2,9 +2,23 @@ import React from "react";
 import { Stack, Typography } from "@mui/material";
 import Icon from "../assets/icons/gym.png";
 
-function BodyPart() {
+function BodyPart({ item, setBodyPart, bodyPart }) {
   return (
-    <Stack type="button">
+    <Stack
+      type="button"
+      alignItems="center"
+      justifyContent="center"
+      className="bodyPart-card"
+      sx={{
+        borderTop: bodyPart === item ? "4px solid #ff2625" : "",
+        backgroundColor: "#fff",
+        borderBottomLeftRadius: "20px",
+        width: "270px",
+        height: "280px",
+        cursor: "pointer",
+        gap: "47px",
+      }}
+    >
       <img
         src={Icon}
         alt="dumbbell"
