51e195cb1f7d3f83b20dd39311a09222f93b28c7
Waldemar Głaz <w.glaz@cheil.com>
2022-12-16 11:05:15 +0100

    .env removed

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Stack, Typography } from "@mui/material";
+import Icon from "../assets/icons/gym.png";
 
-function BodyPart({ item }) {
-  return <div>{item.name}</div>;
+function BodyPart() {
+  return (
+    <Stack type="button">
+      <img
+        src={Icon}
+        alt="dumbbell"
+        styles={{ width: "40px", height: "40px" }}
+      />
+    </Stack>
+  );
 }
 
 export default BodyPart;
