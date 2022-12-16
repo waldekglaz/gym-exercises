0edb7c90e3734b83be399440c4718556bd20ba82
Waldemar Głaz <w.glaz@cheil.com>
2022-12-16 10:57:14 +0100

    HorizontalScrollBar

---
+++ src/components/BodyPart.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function BodyPart({ item }) {
+  return <div>{item.name}</div>;
+}
+
+export default BodyPart;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,22 @@ import HeroBanner from "../components/HeroBanner";
 import SearchExercises from "../components/SearchExercises";
 import Exercises from "../components/Exercises";
 const Home = () => {
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState("all");
+
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises />
-      <Exercises />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
     </Box>
   );
 };
